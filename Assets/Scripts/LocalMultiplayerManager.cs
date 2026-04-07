734ae229b3d0fc286db83439661c0dae1663e79a
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-04-07 10:01:30 -0400

    Got multiplayer working

---
+++ Assets/Scripts/LocalMultiplayerManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LocalMultiplayerManager : MonoBehaviour
+{
+    public List<Sprite> playerSprites;
+    public List<PlayerInput> players;
+
+    public void OnPlayerJoined(PlayerInput player)
+    {
+        players.Add(player);
+
+        SpriteRenderer sr = player.GetComponent<SpriteRenderer>();
+        sr.sprite = playerSprites[player.playerIndex];
+
+        LocalMultiplayerController controller = player.GetComponent<LocalMultiplayerController>();
+        controller.manager = this;
+    }
+
+    public void PlayerAttacking(PlayerInput attackPlayer)
+    {
+        for(int i = 0; i < players.Count; i++)
+        {
+            if (attackPlayer == players[i]) continue;
+
+            if(Vector2.Distance(attackPlayer.transform.position, players[i].transform.position) < 0.5f)
+            {
+                Debug.Log("Player " + attackPlayer.playerIndex + " hit player " + players[i].playerIndex);
+            }
+        }
+        
+    }
+}
